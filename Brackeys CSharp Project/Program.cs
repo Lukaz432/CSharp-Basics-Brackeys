1e27a2a82f5a65e6acee100dfe02ec808b1e1a99
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-20 18:03:16 +0300

    Challenge-Terminal-Conversation
    WriteLine, ReadLine

--- Brackeys CSharp Project/Program.cs
+++ Brackeys CSharp Project/Program.cs
@@ -14,16 +14,35 @@ namespace Brackeys_CSharp_Project
 
             // Conversation
 
-            Console.WriteLine("WHO ARE YOU?!");
+            Console.WriteLine("A confident young man named..");
 
             Console.ReadLine();
 
-            Console.WriteLine("My name is T-1000. \nI am a droid from the future.");
-            Console.WriteLine("What year are you from?");
+            Console.WriteLine("..Stumbled upon Brackey's youtube channel and decided to become a..");
 
             Console.ReadLine();
 
-            Console.WriteLine("I am aware of that. I am from the year 2077.");
+            Console.WriteLine("He then decided to take a sip of..");
+
+            Console.ReadLine();
+
+            Console.WriteLine("..and with all that energy wanted to create a..");
+
+            Console.ReadLine();
+
+            Console.WriteLine("..but realized he does not have the knowledge to do so, so he..");
+
+            Console.ReadLine();
+
+            Console.WriteLine("..yet that was not enough. Instead, he..");
+
+            Console.ReadLine();
+
+            Console.WriteLine("..Although, after all these hours spent, he was full of passion and joy, the man named..");
+
+            Console.ReadLine();
+
+            Console.WriteLine("..just melted into his chair after losing so much sleep..\n \n THE END");
 
             //Makes terminal wait for input before closing
             Console.ReadKey();
