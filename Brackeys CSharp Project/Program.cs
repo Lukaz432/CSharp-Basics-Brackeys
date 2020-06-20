986a514a8327056b23649907767264834a48ba2d
Lukaz432 <55126350+Lukaz432@users.noreply.github.com>
2020-06-20 17:48:02 +0300

    Basics
    WriteLine, ReadLine, ReadKey

--- Brackeys CSharp Project/Program.cs
+++ Brackeys CSharp Project/Program.cs
@@ -6,7 +6,25 @@ namespace Brackeys_CSharp_Project
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Appearance
+
+            Console.Title = "Mega Terminal";
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WindowHeight = 30;
+
+            // Conversation
+
+            Console.WriteLine("WHO ARE YOU?!");
+
+            Console.ReadLine();
+
+            Console.WriteLine("My name is T-1000. \nI am a droid from the future.");
+            Console.WriteLine("What year are you from?");
+
+            Console.ReadLine();
+
+            Console.WriteLine("I am aware of that. I am from the year 2077.");
+
             //Makes terminal wait for input before closing
             Console.ReadKey();
         }
